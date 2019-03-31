17e983cc4a4f06c4c3670645764956ca320dfc90
김대현 <salme4@castis.com>
2019-03-31 22:18:03 +0900

    initial commit

---
+++ src/test/java/my/home/springbootliquibasedemo/SpringbootliquibasedemoApplicationTests.java
@@ -0,0 +1,16 @@
+package my.home.springbootliquibasedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootliquibasedemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
